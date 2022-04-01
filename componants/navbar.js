3becf8db0501c3a8571fb20fcc86cacecea3e6c0
Laxmi Dutta <duttasuravi225@gmail.com>
2022-04-01 14:39:37 +0530

    first commit

---
+++ componants/navbar.js
@@ -0,0 +1,11 @@
+const navbar = ()=>{
+    return `<div id="navbar">
+    <div> 
+        <h3><a href="index.html">HOME</a></h3> </div>
+    <div class="options">
+        <h3>  <a href="recipeOfTheDay.html">Recipe Of The Day </a></h3>
+        <h3> <a href="selectByCategory.html">Select By Catergory</a> </h3>
+    </div>
+</div>`
+}
+export default navbar;
